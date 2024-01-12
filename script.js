22f9060f0a3e4254764a681664471184f66d51ab
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-01-12 00:14:55 -0600

    Add .gitattributes. Complete and refactor JS

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Get elements
 const billInput = document.querySelector(".bill-input");
-const buttons = document.querySelector(".button-container");
+const buttons = document.querySelectorAll(".button-container button");
+const tipOutput = document.querySelector(".tip-total");
+const billOutput = document.querySelector(".bill-total");
+const clearBtn = document.querySelector(".clear");
 
 // Global
 
@@ -16,19 +19,30 @@ function calculateTotalBill(tip, bill) {
 
 // Render Outputs to the DOM
 function renderOutput(tip, totalBill) {
-  // DOM stuff
+  tipOutput.textContent = `$${tip.toFixed(2)}`;
+  billOutput.textContent = `$${totalBill.toFixed(2)}`;
 }
 
-// Handle Tip Amount Buttons
-buttons.addEventListener("click", (e) => {
-  const billAmount = parseFloat(billInput.value);
-  const target = e.target;
-  let percent = parseFloat(target.className.slice(1));
+// Handle Tip Buttons
+for (let button of buttons) {
+  button.addEventListener("click", (e) => {
+    const billAmount = parseFloat(billInput.value);
+    const target = e.target;
+    let percent = parseFloat(target.className.slice(1));
 
-  if (isNaN(billAmount)) {
-    alert("Please enter a valid number");
-  } else {
-    const tip = calculateTip(percent, billAmount);
-    const totalBill = calculateTotalBill(tip, billAmount);
-  }
+    if (isNaN(billAmount)) {
+      alert("Please enter a valid number");
+      billInput.value = "";
+    } else {
+      const tip = calculateTip(percent, billAmount);
+      const totalBill = calculateTotalBill(tip, billAmount);
+      renderOutput(tip, totalBill);
+    }
+  });
+}
+
+// Handle Clear Button
+clearBtn.addEventListener("click", () => {
+  renderOutput(0, 0);
+  billInput.value = "";
 });
